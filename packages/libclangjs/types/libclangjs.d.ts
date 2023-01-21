28abda34374d3bee627882df5fbba41b2732ff94
Sebastian Alff <donalffons@gmail.com>
2023-01-21 21:20:21 +0100

    translation unit related typings

--- packages/libclangjs/types/libclangjs.d.ts
+++ packages/libclangjs/types/libclangjs.d.ts
@@ -1208,6 +1208,162 @@ export type CXLoadDiag_Error = {
 };
 
 /**
+ * Flags that control the creation of translation units.
+ *
+ * The enumerators in this enumeration type are meant to be bitwise
+ * ORed together to specify which options should be used when
+ * constructing the translation unit.
+ */
+export type CXTranslationUnit_Flags = {
+  /**
+   * Used to indicate that no special translation-unit options are
+   * needed.
+   */
+  CXTranslationUnit_None: EnumEntry<CXTranslationUnit_Flags>;
+
+  /**
+   * Used to indicate that the parser should construct a "detailed"
+   * preprocessing record, including all macro definitions and instantiations.
+   *
+   * Constructing a detailed preprocessing record requires more memory
+   * and time to parse, since the information contained in the record
+   * is usually not retained. However, it can be useful for
+   * applications that require more detailed information about the
+   * behavior of the preprocessor.
+   */
+  CXTranslationUnit_DetailedPreprocessingRecord: EnumEntry<CXTranslationUnit_Flags>;
+
+  /**
+   * Used to indicate that the translation unit is incomplete.
+   *
+   * When a translation unit is considered "incomplete", semantic
+   * analysis that is typically performed at the end of the
+   * translation unit will be suppressed. For example, this suppresses
+   * the completion of tentative declarations in C and of
+   * instantiation of implicitly-instantiation function templates in
+   * C++. This option is typically used when parsing a header with the
+   * intent of producing a precompiled header.
+   */
+  CXTranslationUnit_Incomplete: EnumEntry<CXTranslationUnit_Flags>;
+
+  /**
+   * Used to indicate that the translation unit should be built with an
+   * implicit precompiled header for the preamble.
+   *
+   * An implicit precompiled header is used as an optimization when a
+   * particular translation unit is likely to be reparsed many times
+   * when the sources aren't changing that often. In this case, an
+   * implicit precompiled header will be built containing all of the
+   * initial includes at the top of the main file (what we refer to as
+   * the "preamble" of the file). In subsequent parses, if the
+   * preamble or the files in it have not changed, \c
+   * clang_reparseTranslationUnit() will re-use the implicit
+   * precompiled header to improve parsing performance.
+   */
+  CXTranslationUnit_PrecompiledPreamble: EnumEntry<CXTranslationUnit_Flags>;
+
+  /**
+   * Used to indicate that the translation unit should cache some
+   * code-completion results with each reparse of the source file.
+   *
+   * Caching of code-completion results is a performance optimization that
+   * introduces some overhead to reparsing but improves the performance of
+   * code-completion operations.
+   */
+  CXTranslationUnit_CacheCompletionResults: EnumEntry<CXTranslationUnit_Flags>;
+
+  /**
+   * Used to indicate that the translation unit will be serialized with
+   * \c clang_saveTranslationUnit.
+   *
+   * This option is typically used when parsing a header with the intent of
+   * producing a precompiled header.
+   */
+  CXTranslationUnit_ForSerialization: EnumEntry<CXTranslationUnit_Flags>;
+
+  /**
+   * DEPRECATED: Enabled chained precompiled preambles in C++.
+   *
+   * Note: this is a *temporary* option that is available only while
+   * we are testing C++ precompiled preamble support. It is deprecated.
+   */
+  CXTranslationUnit_CXXChainedPCH: EnumEntry<CXTranslationUnit_Flags>;
+
+  /**
+   * Used to indicate that function/method bodies should be skipped while
+   * parsing.
+   *
+   * This option can be used to search for declarations/definitions while
+   * ignoring the usages.
+   */
+  CXTranslationUnit_SkipFunctionBodies: EnumEntry<CXTranslationUnit_Flags>;
+
+  /**
+   * Used to indicate that brief documentation comments should be
+   * included into the set of code completions returned from this translation
+   * unit.
+   */
+  CXTranslationUnit_IncludeBriefCommentsInCodeCompletion: EnumEntry<CXTranslationUnit_Flags>;
+
+  /**
+   * Used to indicate that the precompiled preamble should be created on
+   * the first parse. Otherwise it will be created on the first reparse. This
+   * trades runtime on the first parse (serializing the preamble takes time) for
+   * reduced runtime on the second parse (can now reuse the preamble).
+   */
+  CXTranslationUnit_CreatePreambleOnFirstParse: EnumEntry<CXTranslationUnit_Flags>;
+
+  /**
+   * Do not stop processing when fatal errors are encountered.
+   *
+   * When fatal errors are encountered while parsing a translation unit,
+   * semantic analysis is typically stopped early when compiling code. A common
+   * source for fatal errors are unresolvable include files. For the
+   * purposes of an IDE, this is undesirable behavior and as much information
+   * as possible should be reported. Use this flag to enable this behavior.
+   */
+  CXTranslationUnit_KeepGoing: EnumEntry<CXTranslationUnit_Flags>;
+
+  /**
+   * Sets the preprocessor in a mode for parsing a single file only.
+   */
+  CXTranslationUnit_SingleFileParse: EnumEntry<CXTranslationUnit_Flags>;
+
+  /**
+   * Used in combination with CXTranslationUnit_SkipFunctionBodies to
+   * constrain the skipping of function bodies to the preamble.
+   *
+   * The function bodies of the main file are not skipped.
+   */
+  CXTranslationUnit_LimitSkipFunctionBodiesToPreamble: EnumEntry<CXTranslationUnit_Flags>;
+
+  /**
+   * Used to indicate that attributed types should be included in CXType.
+   */
+  CXTranslationUnit_IncludeAttributedTypes: EnumEntry<CXTranslationUnit_Flags>;
+
+  /**
+   * Used to indicate that implicit attributes should be visited.
+   */
+  CXTranslationUnit_VisitImplicitAttributes: EnumEntry<CXTranslationUnit_Flags>;
+
+  /**
+   * Used to indicate that non-errors from included files should be ignored.
+   *
+   * If set, clang_getDiagnosticSetFromTU() will not report e.g. warnings from
+   * included files anymore. This speeds up clang_getDiagnosticSetFromTU() for
+   * the case where these warnings are not of interest, as for an IDE for
+   * example, which typically shows only the diagnostics in the main file.
+   */
+  CXTranslationUnit_IgnoreNonErrorsFromIncludedFiles: EnumEntry<CXTranslationUnit_Flags>;
+
+  /**
+   * Tells the preprocessor not to skip excluded conditional blocks.
+   */
+  CXTranslationUnit_RetainExcludedConditionalBlocks: EnumEntry<CXTranslationUnit_Flags>;
+};
+
+/**
  * Visitor invoked for each cursor found by a traversal.
  *
  * This visitor function will be invoked for each cursor found by
@@ -1495,7 +1651,76 @@ export type LibClang = EmscriptenModule & {
   // skipped clang_getDiagnosticNumFixIts
   // skipped clang_getDiagnosticFixIt
 
-  // ################# TODO: skipped some functions
+  /**
+   * Get the original translation unit source file name.
+   */
+  clang_getTranslationUnitSpelling: (CTUnit: CXTranslationUnit) => string;
+
+  /**
+   * Return the CXTranslationUnit for a given source file and the provided
+   * command line arguments one would pass to the compiler.
+   *
+   * Note: The 'source_filename' argument is optional.  If the caller provides a
+   * NULL pointer, the name of the source file is expected to reside in the
+   * specified command line arguments.
+   *
+   * Note: When encountered in 'clang_command_line_args', the following options
+   * are ignored:
+   *
+   *   '-c'
+   *   '-emit-ast'
+   *   '-fsyntax-only'
+   *   '-o \<output file>'  (both '-o' and '\<output file>' are ignored)
+   *
+   * @param CIdx The index object with which the translation unit will be
+   * associated.
+   *
+   * @param source_filename The name of the source file to load, or NULL if the
+   * source file is included in \p clang_command_line_args.
+   *
+   * @param num_clang_command_line_args The number of command-line arguments in
+   * clang_command_line_args.
+   *
+   * @param clang_command_line_args The command-line arguments that would be
+   * passed to the \c clang executable if it were being invoked out-of-process.
+   * These command-line options will be parsed and will affect how the translation
+   * unit is parsed. Note that the following options are ignored: '-c',
+   * '-emit-ast', '-fsyntax-only' (which is the default), and '-o \<output file>'.
+   *
+   * @param num_unsaved_files the number of unsaved file entries in \p
+   * unsaved_files.
+   *
+   * @param unsaved_files the files that have not yet been saved to disk
+   * but may be required for code completion, including the contents of
+   * those files.  The contents and name of these files (as specified by
+   * CXUnsavedFile) are copied when necessary, so the client only needs to
+   * guarantee their validity until the call to this function returns.
+   */
+  clang_createTranslationUnitFromSourceFile: (CIdx: CXIndex, source_filename: null | string, clang_command_line_args: string[] | null, unsaved_files: CXUnsavedFile[] | null) => CXTranslationUnit;
+
+  /**
+   * Same as {@link LibClang.clang_createTranslationUnit2 | clang_createTranslationUnit2}, but returns
+   * the \c CXTranslationUnit instead of an error code.  In case of an error this
+   * routine returns a \c NULL \c CXTranslationUnit, without further detailed
+   * error codes.
+   */
+  clang_createTranslationUnit: (CIdx: CXIndex, ast_filename: string | null) => CXTranslationUnit;
+
+  // skipped clang_createTranslationUnit2
+
+  /**
+   * Returns the set of flags that is suitable for parsing a translation
+   * unit that is being edited.
+   *
+   * The set of flags returned provide options for {@link clang_parseTranslationUnit | clang_parseTranslationUnit()}
+   * to indicate that the translation unit is likely to be reparsed many times,
+   * either explicitly (via {@link clang_reparseTranslationUnit | clang_reparseTranslationUnit()}) or implicitly
+   * (e.g., by code completion ({@link clang_codeCompletionAt | clang_codeCompletionAt()})). The returned flag
+   * set contains an unspecified set of optimizations (e.g., the precompiled
+   * preamble) geared toward improving the performance of these routines. The
+   * set of optimizations enabled may change from one version to the next.
+   */
+  clang_defaultEditingTranslationUnitOptions: () => void;
 
   /**
    * Same as `clang_parseTranslationUnit2`, but returns
