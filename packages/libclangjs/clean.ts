1c800c29dea42adea5f90e3d7887504dec0efd22
Sebastian Alff <donalffons@gmail.com>
2023-01-22 16:03:42 +0100

    update clean script

--- packages/libclangjs/clean.ts
+++ packages/libclangjs/clean.ts
@@ -1,4 +1,7 @@
 import fs from "fs";
 
 fs.rmSync("build", { recursive: true, force: true });
-fs.rmSync("dist", { recursive: true, force: true });
+fs.rmSync("libclangjs-node.js", { force: true });
+fs.rmSync("libclangjs-node.wasm", { force: true });
+fs.rmSync("libclangjs-web.js", { force: true });
+fs.rmSync("libclangjs-web.wasm", { force: true });
