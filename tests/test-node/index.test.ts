75fa6ceadde18c03fe0ec02ae1f1b6bdb0c8045d
Sebastian Alff <donalffons@gmail.com>
2023-01-22 10:53:50 +0100

    cleanup

--- tests/test-node/index.test.ts
+++ tests/test-node/index.test.ts
@@ -20,7 +20,7 @@ test("Can create libclang index ", async () => {
   index = libclangjs.clang_createIndex(1, 1);
 });
 
-test(`call "clang_CXIndex_setInvocationEmissionPathOption" with both string and null as second parameter`, async () => {
+test(`Call "clang_CXIndex_setInvocationEmissionPathOption" with both string and null as second parameter`, async () => {
   libclangjs.clang_CXIndex_setInvocationEmissionPathOption(index, null);
   fs.mkdirSync(path.join("testSrc", "log"), { recursive: true });
   libclangjs.clang_CXIndex_setInvocationEmissionPathOption(index, path.join(cwd, "log"));
@@ -28,7 +28,7 @@ test(`call "clang_CXIndex_setInvocationEmissionPathOption" with both string and 
 
 let tu: CXTranslationUnit;
 
-test(`call "clang_parseTranslationUnit"`, async () => {
+test(`Call "clang_parseTranslationUnit"`, async () => {
   const nullTu = libclangjs.clang_parseTranslationUnit(index, "nonexistingfile", null, null, 0);
   expect(libclangjs.isNullPointer(nullTu)).toBeTruthy();
   tu = libclangjs.clang_parseTranslationUnit(index, path.join(cwd, "main.cpp"), [`-I${path.join(cwd, "dir")}`], null, 0);
@@ -46,13 +46,13 @@ test(`call "clang_parseTranslationUnit"`, async () => {
 
 let mainFile: CXFile;
 
-test("can get file contents", () => {
+test("Can get file contents", () => {
   mainFile = libclangjs.clang_getFile(tu, path.join(cwd, "main.cpp"));
   const fileContents = libclangjs.clang_getFileContents(tu, mainFile);
   expect(fileContents).toBe(fs.readFileSync(path.join("testSrc", "main.cpp")).toString());
 });
 
-test("can make simple calls regarding CXSourceLocation", () => {
+test("Can make simple calls regarding CXSourceLocation", () => {
   const nullLoc = libclangjs.clang_getNullLocation();
   const loc1 = libclangjs.clang_getLocation(tu, mainFile, 0, 1);
   const loc2 = libclangjs.clang_getLocation(tu, mainFile, 0, 2);
@@ -62,8 +62,7 @@ test("can make simple calls regarding CXSourceLocation", () => {
   expect(libclangjs.clang_equalLocations(loc1, loc3)).toBe(0);
 });
 
-test("can make simple calls regarding CXSourceRange", () => {
-  const nullRange = libclangjs.clang_getNullRange();
+test("Can make simple calls regarding CXSourceRange", () => {
   const loc1 = libclangjs.clang_getLocation(tu, mainFile, 0, 1);
   const loc2 = libclangjs.clang_getLocation(tu, mainFile, 0, 2);
   const loc3 = libclangjs.clang_getLocation(tu, mainFile, 3, 1);
