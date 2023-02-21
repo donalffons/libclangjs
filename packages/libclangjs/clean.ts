cfc1b5746bac71188d019e758fd8e5de53c20605
Sebastian Alff <donalffons@gmail.com>
2023-02-21 23:08:44 +0100

    add / update clean scripts

--- packages/libclangjs/clean.ts
+++ packages/libclangjs/clean.ts
@@ -2,7 +2,8 @@ import fs from "fs";
 
 fs.rmSync("build", { recursive: true, force: true });
 fs.rmSync("node.js", { force: true });
-fs.rmSync("node.wasm", { force: true });
+fs.rmSync("node.worker.js", { force: true });
 fs.rmSync("web.js", { force: true });
-fs.rmSync("web.wasm", { force: true });
+fs.rmSync("web.worker.js", { force: true });
+fs.rmSync("libclang.wasm", { force: true });
 fs.rmSync("README.md", { force: true });
